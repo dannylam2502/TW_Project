53967371abb6381f8ffcbc67358fa317e9280ee1
kvtuyen0315 <41115914+kvtuyen0315@users.noreply.github.com>
2022-04-21 01:33:13 +0700

    *Add project C++.

---
+++ SourceCode/Source/TW_Project/TW_ProjectGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "TW_ProjectGameModeBase.h"
+
+ATW_ProjectGameModeBase::ATW_ProjectGameModeBase()
+{
+	DefaultPawnClass = AMyCharacter::StaticClass();
+}
---
+++ SourceCode/Source/TW_Project/TW_ProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyCharacter.h"
+#include "TW_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TW_PROJECT_API ATW_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ATW_ProjectGameModeBase();
+};
